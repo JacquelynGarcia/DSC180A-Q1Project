e27aa80a1cf33cf03fa86d2ac94dc00165322ef2
amelialei <aclei@ucsd.edu>
2025-11-04 18:42:37 -0800

    add data path to config

--- src/script.py
+++ src/script.py
@@ -1,13 +1,17 @@
+import json
 from code import load_datasets
 
 def main():
-    train_path = "data/train2.tsv"
-    val_path   = "data/val2.tsv"
-    test_path  = "data/test2.tsv"
+    with open("src/config.json", "r") as f:
+        cfg = json.load(f)
+
+    train_path = cfg["train_path"]
+    val_path = cfg["val_path"]
+    test_path = cfg["test_path"]
 
     df_train, df_val, df_test = load_datasets(train_path, val_path, test_path)
-    
-    
+
+
 
 if __name__ == "__main__":
     main()
